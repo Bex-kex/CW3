70cf5ec1597a211b9b6b5933152b379d985c268f
Will Coleman <xk22276@bristol.ac.uk>
2023-04-22 20:01:36 +0100

    added functionality to read and write grids to and from a .txt file

--- Coursework 3.py
+++ Coursework 3.py
@@ -46,7 +46,7 @@ gridtest = [
     [0, 9, 4, 0, 8, 0, 0, 0, 0],
     [0, 0, 3, 7, 0, 8, 4, 0, 9],
     [0, 4, 0, 0, 1, 3, 7, 0, 6],
-    [0, 4, 0, 0, 1, 3, 7, 0, 6]
+    [0, 6, 0, 9, 0, 0, 1, 0, 8]
 ]
 
 
@@ -172,6 +172,7 @@ def get_possible(grid, n_loc_rows, n_loc_cols, n_rows, n_cols, i, j):
 
 def recursive_solve(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
     global depth
+    global EXPLAIN
     empty = find_empty(grid, n_rows, n_cols)
 
     if not empty:
@@ -202,29 +203,93 @@ def recursive_solve(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
 #       check_solution(grid9, 3, 2, 6, 6),
 #       check_solution(grid10, 3, 2, 6, 6))
 depth = 0
+def parsed() -> tuple:
+    """
+    simple function which parses the grid in the file provided,
+    into a nested list which is compatible with the solver.
+    also determines the dimensions of the given grid.
+    """
+    output_list = []
+    with open(FILE_IN) as file:
+        #opening the txt file specified by the FILE_IN cmdline argument
+        for i in file.readlines():
+            #reading each line of the txt file and formatting the grid
+            line:list = [int(j) for j in i.strip('\n').split(', ')]
+            output_list.append(line)
+            #appending the formatted list to the final output list
 
-def main(CLA):
+    main_dims = len(output_list[0])
+    subgrid_dims = int(main_dims**(1/2))
+            
+    return (output_list,subgrid_dims,subgrid_dims,main_dims,main_dims)
+
+
+def write_to_file(ans:list,explanation=None) -> None:
+    """
+    This function takes the answer grid and also if available the explanation of the answer,
+    and writes it to a new file, the filepath and filename given in the cmdline argument:
+    FILE_OUT
+    """
+    with open(FILE_OUT,'w') as output_file:
+        for line in ans:
+            line = str(line).strip('[]') + '\n'
+            output_file.writelines(line)
+        if explanation:
+            for i in explanation:
+                output_file.writelines(i)
+    print(f'Output saved to \"{FILE_OUT}\"')
+
+
+
+
+def main(CLA:list)-> None:
+    """
+    Wrapper function which parses the command line arguments and runs
+    the main script with the users selected settings, (e.g. hints=on, profiling=on)
+    """
+    global EXPLAIN
+    global FILE_IN
+    global FILE_OUT
+    global HINT
+    global PROFILE
+    default_args = [False,None,None,0,False]
     #take command-line arguments and parse them into a dict
+    if not CLA:
+        EXPLAIN,FILE_IN,FILE_OUT,HINT,PROFILE = default_args
+        print(recursive_solve(gridtest,3,3,9,9))
     args: dict = vars(parser.parse_args(CLA))
-    print(args)
+    
     for i in args.keys():
         #turning dict values into constants to be used in rest of program
         match i:
+            #assigning constants values, based on what was inputted as arguments
             case 'explain':
-                EXPLAIN: bool = args.get(i)
+                EXPLAIN = args.get(i)
+
             case 'file':
-                FILE_IN: str
-                FILE_OUT: str
-                FILE_IN, FILE_OUT = args.get(i)
+                try:
+                    FILE_IN, FILE_OUT = args.get(i)
+
+                except TypeError:
+                    FILE_IN,FILE_OUT = None, None
+
             case 'hint':
-                HINT = int(args.get(i).pop())
+                try:
+                    HINT = int(args.get(i).pop())
+
+                except:
+                    HINT = 0
+
             case 'profile':
-                PROFILE:bool = args.get(i)
+                PROFILE = args.get(i)
     if FILE_IN:
-        write_to_file(recursive_solve(parsed()))
-    if PROFILE:...  
-    else:
-        print(recursive_solve)
+        #if there is a filepath given, solve the sudoku in the text file and...
+        #save the output to a separate text file
+        write_to_file(recursive_solve(*parsed()))
+      
+    else:...
+        #else, just print the results to the terminal
+        #print(recursive_solve(gridtest,3,3,9,9))
     
     
     #print(recursive_solve(gridtest, 3, 3, 9, 9))
@@ -236,8 +301,9 @@ if __name__ == '__main__':
     parser.add_argument("--file",nargs=2,action='extend')
     parser.add_argument("--hint",nargs=1,action='store')
     parser.add_argument("-p","--profile",action='store_true')
-
-    main(sys.argv[1:])
+    test_args = ['--file','grids/easy1.txt','out.txt']
+    main(test_args)
+    #main(sys.argv[1:])
 
 
 
