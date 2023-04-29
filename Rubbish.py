b755d7e627ad633420e807bc29e0b76f3f4fd82d
Bex-kex <hf22142@bristol.ac.uk>
2023-04-29 20:51:02 +0100

    Updated the Rubbish file

--- Rubbish.py
+++ Rubbish.py
@@ -1,42 +1,67 @@
 import time
 
-grid1 = [
-    [1, 0, 4, 2],
-    [4, 2, 1, 3],
-    [2, 1, 3, 4],
-    [3, 4, 2, 1]]
+easy1 = [
+    [9, 0, 6, 0, 0, 1, 0, 4, 0],
+    [7, 0, 1, 2, 9, 0, 0, 6, 0],
+    [4, 0, 2, 8, 0, 6, 3, 0, 0],
+    [0, 0, 0, 0, 2, 0, 9, 8, 0],
+    [6, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 9, 4, 0, 8, 0, 0, 0, 0],
+    [0, 0, 3, 7, 0, 8, 4, 0, 9],
+    [0, 4, 0, 0, 1, 3, 7, 0, 6],
+    [0, 6, 0, 9, 0, 0, 1, 0, 8]]
 
-grid2 = [
-    [1, 0, 4, 2],
-    [4, 2, 1, 3],
-    [2, 1, 0, 4],
-    [3, 4, 2, 1]]
+easy2 = [
+    [0, 0, 0, 2, 6, 0, 7, 0, 1],
+    [6, 8, 0, 0, 7, 0, 0, 9, 0],
+    [1, 9, 0, 0, 0, 4, 5, 0, 0],
+    [8, 2, 0, 1, 0, 0, 0, 4, 0],
+    [0, 0, 4, 6, 0, 2, 9, 0, 0],
+    [0, 5, 0, 0, 0, 3, 0, 2, 8],
+    [0, 0, 9, 3, 0, 0, 0, 7, 4],
+    [0, 4, 0, 0, 5, 0, 0, 3, 6],
+    [7, 0, 3, 0, 1, 8, 0, 0, 0]]
 
-grid3 = [
-    [1, 0, 4, 2],
-    [4, 2, 1, 0],
-    [2, 1, 0, 4],
-    [0, 4, 2, 1]]
+easy3 = [
+    [0, 3, 0, 4, 0, 0],
+    [0, 0, 5, 6, 0, 3],
+    [0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 3, 0, 5],
+    [0, 6, 4, 0, 3, 1],
+    [0, 0, 1, 0, 4, 6]]
 
-grid4 = [
-    [1, 0, 4, 2],
-    [0, 2, 1, 0],
-    [2, 1, 0, 4],
-    [0, 4, 2, 1]]
+hard1 = [
+    [0, 2, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 6, 0, 4, 0, 0, 0, 0],
+    [5, 8, 0, 0, 9, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 3, 0, 0, 4],
+    [4, 1, 0, 0, 8, 0, 6, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 9, 5],
+    [2, 0, 0, 0, 1, 0, 0, 8, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 1, 0, 0, 8, 0, 5, 7]]
 
-grid5 = [
-    [1, 0, 0, 2],
-    [0, 0, 1, 0],
-    [0, 1, 0, 4],
-    [0, 0, 0, 1]]
+med1 = [
+    [0, 0, 0, 6, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 5, 0, 1],
+    [3, 6, 9, 0, 8, 0, 4, 0, 0],
+    [0, 0, 0, 0, 0, 6, 8, 0, 0],
+    [0, 0, 0, 1, 3, 0, 0, 0, 9],
+    [4, 0, 5, 0, 0, 9, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 3, 0, 0],
+    [0, 0, 6, 0, 0, 7, 0, 0, 0],
+    [1, 0, 0, 3, 4, 0, 0, 0, 0]]
 
-grid6 = [
-    [0, 0, 6, 0, 0, 3],
-    [5, 0, 0, 0, 0, 0],
-    [0, 1, 3, 4, 0, 0],
-    [0, 0, 0, 0, 0, 6],
-    [0, 0, 1, 0, 0, 0],
-    [0, 5, 0, 0, 6, 4]]
+med2 = [
+    [8, 0, 9, 0, 2, 0, 3, 0, 0],
+    [0, 3, 7, 0, 6, 0, 5, 0, 0],
+    [0, 0, 0, 4, 0, 9, 7, 0, 0],
+    [0, 0, 2, 9, 0, 1, 0, 6, 0],
+    [1, 0, 0, 3, 0, 6, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 3],
+    [7, 0, 0, 0, 0, 0, 0, 0, 8],
+    [5, 0, 0, 0, 0, 0, 0, 1, 4],
+    [0, 0, 0, 2, 8, 4, 6, 0, 5]]
 
 
 def check_section(section, n):
@@ -46,19 +71,21 @@ def check_section(section, n):
     return False
 
 
+def split_into_squares(sub_grid, n):
+    split = []
+    boundary = 0
+    for i in range(sub_grid):
+        split.append(boundary)
+        boundary += n // sub_grid
+
+    return split
+
+
 def get_squares(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
     squares = []
-    row_split = []
-    col_split = []
-    boundary = 0
 
-    for i in range(n_loc_rows):
-        row_split.append(boundary)
-        boundary += n_rows // n_loc_rows
-    boundary = 0
-    for j in range(n_loc_cols):
-        col_split.append(boundary)
-        boundary += n_cols // n_loc_cols
+    row_split = split_into_squares(n_loc_rows, n_rows)
+    col_split = split_into_squares(n_loc_cols, n_cols)
 
     for row in row_split:
         for col in col_split:
@@ -78,11 +105,9 @@ def check_solution(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
             return False
 
     for i in range(n_rows):
-
         column = []
         for row in grid:
             column.append(row[i])
-
         if not check_section(column, n):
             return False
 
@@ -104,6 +129,7 @@ def bubble_sort(any_list):
                 any_list[j + 1] = buffer
 
     return any_list
+
 
 def find_empty(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
     zeros_outer = []
@@ -129,7 +155,7 @@ def find_empty(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
     return zeros_sorted
 
 
-def get_all(grid, n_loc_rows, n_loc_cols, i, j, n_rows, n_cols):
+def get_all(grid, n_loc_rows, n_loc_cols, n_rows, n_cols, i, j):
     row = []
     for k in range(n_cols):
         if grid[i][k] != 0:
@@ -156,16 +182,16 @@ def get_all(grid, n_loc_rows, n_loc_cols, i, j, n_rows, n_cols):
         if i >= k:
             index = row_split.index(k)
     boundary_i = row_split[index]
-    for k in row_split:
+    for k in col_split:
         if j >= k:
-            index = row_split.index(k)
-    boundary_j = row_split[index]
+            index = col_split.index(k)
+    boundary_j = col_split[index]
 
     square = []
-    for i in range(n_rows // n_loc_rows):
-        for j in range(n_cols // n_loc_cols):
-            if grid[boundary_i + i][boundary_j + j] != 0:
-                square.append(grid[boundary_i + i][boundary_j + j])
+    for q in range(n_rows // n_loc_rows):
+        for w in range(n_cols // n_loc_cols):
+            if grid[boundary_i + q][boundary_j + w] != 0:
+                square.append(grid[boundary_i + q][boundary_j + w])
 
     all_present = row + col + square
 
@@ -174,14 +200,14 @@ def get_all(grid, n_loc_rows, n_loc_cols, i, j, n_rows, n_cols):
 
 def get_possible(grid, n_loc_rows, n_loc_cols, n_rows, n_cols, i, j):
     all_possible = [i for i in range(1, n_rows + 1)]
-    all_present = get_all(grid, n_loc_rows, n_loc_cols, i, j, n_rows, n_cols)
+    all_present = get_all(grid, n_loc_rows, n_loc_cols, n_rows, n_cols, i, j)
     only_suitable = list(set(all_possible) - set(all_present))
 
     return only_suitable
 
 
 def recursive_solve(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
-    zeros = find_empty(grid, n_rows, n_cols, n_rows, n_cols)
+    zeros = find_empty(grid, n_loc_rows, n_loc_cols, n_rows, n_cols)
 
     if not zeros:
         if check_solution(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
@@ -207,10 +233,6 @@ def recursive_solve(grid, n_loc_rows, n_loc_cols, n_rows, n_cols):
 
 
 start_time = time.time()
-print(recursive_solve(grid1, 2, 2, 4, 4))
-print(recursive_solve(grid2, 2, 2, 4, 4))
-print(recursive_solve(grid3, 2, 2, 4, 4))
-print(recursive_solve(grid4, 2, 2, 4, 4))
-print(recursive_solve(grid5, 2, 2, 4, 4))
+print(recursive_solve(easy3, 3, 2, 6, 6))
 elapsed_time = time.time() - start_time
 print(elapsed_time)
