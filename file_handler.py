45a8bc88c1a87c019ded99defcdc9e589cfc821f
Will Coleman <xk22276@bristol.ac.uk>
2023-05-05 11:41:35 +0100

    fixed bug with 3x2 grids incorrect dimensions

--- file_handler.py
+++ file_handler.py
@@ -24,7 +24,7 @@ def read_grid_from_file(file_name: str) -> list:
         subgrid_dims_y = 2
         subgrid_dims_x = 3
 
-    return (grid, subgrid_dims_x, subgrid_dims_y)
+    return (grid, subgrid_dims_y, subgrid_dims_x)
 
 
 def print_grid(grid: list, **kwargs) -> str:
